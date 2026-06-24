567653f9f22a966cd3805df516b757dd1c96d782
antoniofernandes-dev <antoniofreirefernandes@gmail.com>
2026-06-24 20:37:41 +0100

    Update count-GC.py

--- count-GC.py
+++ count-GC.py
@@ -1,18 +1,21 @@
 sequence=("GCATGCATGCATGCATGGCGCTAGCGATGCGGCGCGTAGCTTTTGAAGC")
-numGC=0
-numtotal=0
-
-for i in sequence:
-
-    if(i=="G"):
-        numGC+=3
-        numtotal+=3
-    if(i=="C"):
-        numGC+=3
-        numtotal += 3
-    if (i == "A"):
-        numtotal += 2
-    if (i == "T"):
-        numtotal += 2
-GC_content=(numGC/numtotal)*100
-print(f"GC content is:{GC_content}%")
+num_total=len(sequence)
+numA=0
+numC=0
+numG=0
+numT=0
+for nucleotide in sequence:
+    if nucleotide=="A":
+        numA+=1
+    if nucleotide=="C":
+        numC+=1
+    if nucleotide=="G":
+        numG+=1
+    if nucleotide=="T":
+        numT+=1
+gc_perc=((numG+numC)/num_total)*100
+print(f'Nucleotide A: {numA}')
+print(f'Nucleotide C: {numC}')
+print(f'Nucleotide G: {numG}')
+print(f'Nucleotide T: {numT}')
+print(f'GC: {gc_perc}%')
