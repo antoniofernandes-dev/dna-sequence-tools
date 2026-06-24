0e92ad2240a7840b0682dc44d752a012d55684c9
antoniofernandes-dev <antoniofreirefernandes@gmail.com>
2026-06-24 19:52:59 +0100

    Update count-GC.py

--- count-GC.py
+++ count-GC.py
@@ -1,1 +1,18 @@
+sequence=("GCATGCATGCATGCATGGCGCTAGCGATGCGGCGCGTAGCTTTTGAAGC")
+numGC=0
+numtotal=0
 
+for i in sequence:
+
+    if(i=="G"):
+        numGC+=3
+        numtotal+=3
+    if(i=="C"):
+        numGC+=3
+        numtotal += 3
+    if (i == "A"):
+        numtotal += 2
+    if (i == "T"):
+        numtotal += 2
+GC_content=(numGC/numtotal)*100
+print(f"GC content is:{GC_content}%")
